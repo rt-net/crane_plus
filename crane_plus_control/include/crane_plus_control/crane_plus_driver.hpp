c69fad63702c175425080d40c7d21fa7e3b6dea6
ShotaAk <s.aoki@rt-net.jp>
2020-09-17 19:26:29 +0900

    Read servo positions

--- crane_plus_control/include/crane_plus_control/crane_plus_driver.hpp
+++ crane_plus_control/include/crane_plus_control/crane_plus_driver.hpp
@@ -1,10 +1,12 @@
 
 #include <controller_interface/controller_interface.hpp>
+#include <dynamixel_sdk/dynamixel_sdk.h>
 #include <hardware_interface/joint_command_handle.hpp>
 #include <hardware_interface/joint_state_handle.hpp>
 #include <hardware_interface/robot_hardware.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <string>
+
 
 class CranePlusDriver : public hardware_interface::RobotHardware
 {
@@ -21,11 +23,13 @@ private:
   std::vector<double> cmd_;
   std::vector<hardware_interface::OperationMode> op_mode_;
 
+  dynamixel::PortHandler *dxl_port_handler_;
+  dynamixel::PacketHandler *dxl_packet_handler_;
+
 public:
   CranePlusDriver();
   ~CranePlusDriver();
   hardware_interface::return_type init();
-  hardware_interface::return_type update();
   hardware_interface::return_type read();
   hardware_interface::return_type write();
 };
--- crane_plus_control/src/crane_plus_driver.cpp
+++ crane_plus_control/src/crane_plus_driver.cpp
@@ -1,7 +1,7 @@
 
 #include "crane_plus_control/crane_plus_driver.hpp"
 
-// static const rclcpp::Logger LOGGER = rclcpp::get_logger("fake_joint_driver");
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("crane_plus_driver");
 
 CranePlusDriver::CranePlusDriver()
 {
@@ -9,14 +9,27 @@ CranePlusDriver::CranePlusDriver()
 
 CranePlusDriver::~CranePlusDriver()
 {
+  dxl_port_handler_->closePort();
 }
 
 hardware_interface::return_type CranePlusDriver::init()
 {
+  dxl_port_handler_ = dynamixel::PortHandler::getPortHandler("/dev/ttyUSB0");
+  dxl_packet_handler_ = dynamixel::PacketHandler::getPacketHandler(1.0);
+
+  if(!dxl_port_handler_->openPort()){
+    throw std::runtime_error("unable to open dynamixel port");
+  }
+
+  if(!dxl_port_handler_->setBaudRate(1000000)){
+    throw std::runtime_error("unable to set baudrate");
+  }
+
   joint_names_.push_back("crane_plus_joint1");
   joint_names_.push_back("crane_plus_joint2");
   joint_names_.push_back("crane_plus_joint3");
   joint_names_.push_back("crane_plus_joint4");
+  joint_names_.push_back("crane_plus_joint_hand");
 
   // Resize members
   pos_.resize(joint_names_.size());
@@ -57,14 +70,38 @@ hardware_interface::return_type CranePlusDriver::init()
   return hardware_interface::return_type::OK;
 }
 
-hardware_interface::return_type CranePlusDriver::update()
-{
-  pos_ = cmd_;
-  return hardware_interface::return_type::OK;
-}
-
 hardware_interface::return_type CranePlusDriver::read()
 {
+  const int ADDR_MX_PRESENT_POSITION = 36;
+  const double TO_RADIANS = (300.0 / 1023.0) * M_PI / 180.0;
+  static int count=0;
+
+  for(size_t i=0; i < joint_names_.size(); ++i){
+    int dxl_id = i + 1;  // サーボのIDは1 ~ 5なので
+    uint16_t dxl_present_position = 0;
+    uint8_t dxl_error = 0;     
+    int dxl_comm_result = dxl_packet_handler_->read2ByteTxRx(dxl_port_handler_, 
+      dxl_id, ADDR_MX_PRESENT_POSITION, &dxl_present_position, &dxl_error);
+
+    if (dxl_comm_result != COMM_SUCCESS)
+    {
+      RCLCPP_ERROR(LOGGER, "TxRxResult: %d", dxl_comm_result);
+    }
+    else if (dxl_error != 0)
+    {
+      RCLCPP_ERROR(LOGGER, "RxPAcketError: %d", dxl_error);
+    }
+
+    // RCLCPP_INFO(LOGGER, "[ID:%03d] PresPos:%f", dxl_id, dxl_present_position * TO_RADIANS);
+
+    pos_[i] = (dxl_present_position - 511) * TO_RADIANS;
+  }
+
+  count++;
+
+  if(count > 200){
+    count = 0;
+  }
 
   return hardware_interface::return_type::OK;
 }
