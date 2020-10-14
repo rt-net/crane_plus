840d2dbf20c90278171e877783763938feb4ba77
ShotaAk <s.aoki@rt-net.jp>
2020-10-14 17:41:53 +0900

    Refactor crane_plus_description

--- crane_plus_description/launch/display.launch.py
+++ crane_plus_description/launch/display.launch.py
@@ -1,18 +1,16 @@
-
-
-# <launch>
-#   <arg name="gui" default="true"/>
-#   <arg name="state_rate" default="10"/>
-#   <param name="rate" value="$(arg state_rate)"/>
-#   <param name="robot_description"
-#     command="$(find xacro)/xacro --inorder '$(find crane_x7_description)/urdf/crane_x7.urdf.xacro'"
-#     />
-#   <rosparam param="source_list">["joint_states_source"]</rosparam>
-#   <node name="joint_state_publisher_gui" pkg="joint_state_publisher_gui" type="joint_state_publisher_gui" if="$(arg gui)"/>
-#   <node name="joint_state_publisher" pkg="joint_state_publisher" type="joint_state_publisher" unless="$(arg gui)"/>
-#   <node name="robot_state_publisher" pkg="robot_state_publisher" type="robot_state_publisher"/>
-#   <node name="rviz" pkg="rviz" type="rviz" args="-d $(find crane_x7_description)/config/urdf.rviz"/>
-# </launch>
+# Copyright 2020 RT Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 
