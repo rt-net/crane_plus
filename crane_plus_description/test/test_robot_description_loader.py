60da8d967c4b94dc6a779c282a8ec013b782e73a
ShotaAk <s.aoki@rt-net.jp>
2022-06-07 13:44:10 +0900

    robot_description_loaderのテストを追加

---
+++ crane_plus_description/test/test_robot_description_loader.py
@@ -0,0 +1,52 @@
+# Copyright 2022 RT Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from crane_plus_description.robot_description_loader import RobotDescriptionLoader
+from launch.launch_context import LaunchContext
+import pytest
+
+
+def exec_load(loader):
+    # Command substitutionの実行方法はCommandのテストを参考にした
+    # https://github.com/ros2/launch/blob/074cd2903ddccd61bce8f40a0f58da0b7c200481/launch/test/launch/substitutions/test_command.py#L47
+    context = LaunchContext()
+    return loader.load()['robot_description'].perform(context)
+
+
+def test_load_description():
+    # xacroの読み込みが成功することを期待
+    rdl = RobotDescriptionLoader()
+    assert exec_load(rdl)
+
+
+def test_param_size():
+    # パラメータのサイズが1つであることを期待
+    rdl = RobotDescriptionLoader()
+    assert len(rdl.load()) == 1
+
+
+def test_change_param_name():
+    # パラメータ名を変更できることを期待
+    rdl = RobotDescriptionLoader()
+    rdl.param_name = 'test_name'
+    assert 'test_name' in rdl.load().keys()
+
+
+def test_change_description_path():
+    # xacroのファイルパスを変更し、読み込みが失敗することを期待
+    rdl = RobotDescriptionLoader()
+    rdl.robot_description_path = 'hoge'
+    with pytest.raises(Exception) as e:
+        exec_load(rdl)
+    assert e.value
