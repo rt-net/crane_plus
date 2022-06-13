647fd984c0a9a128351c0adc78f178692a31482f
ShotaAk <s.aoki@rt-net.jp>
2022-06-13 18:57:22 +0900

    Linterのチェックを通すための修正

--- crane_plus_ignition/launch/crane_plus_ignition.launch.py
+++ crane_plus_ignition/launch/crane_plus_ignition.launch.py
@@ -17,58 +17,27 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from crane_plus_description.robot_description_loader import RobotDescriptionLoader
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch.actions import ExecuteProcess
 from launch.actions import IncludeLaunchDescription
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # world_file = os.path.join(get_package_share_directory(
-    #     'crane_plus_gazebo'), 'worlds', 'table.world')
-
-    # declare_arg_gui = DeclareLaunchArgument(
-    #     'gui',
-    #     default_value='true',
-    #     description='Set to "false" to run headless.')
-
-    # declare_arg_server = DeclareLaunchArgument(
-    #     'server',
-    #     default_value='true',
-    #     description='Set to "false" not to run gzserver.')
-
-    # xacro_file = os.path.join(get_package_share_directory('crane_plus_description'),
-    #                           'urdf', 'crane_plus.urdf.xacro')
-    # robot_description = {'robot_description': Command(
-    #     ['xacro ', xacro_file, ' use_gazebo:=', 'false'])}
-
-    # node_robot_state_publisher = Node(
-    #     package='robot_state_publisher',
-    #     executable='robot_state_publisher',
-    #     output='screen',
-    #     parameters=[robot_description]
-    # )
-
-    # launch_ignition = IncludeLaunchDescription(
-    #         PythonLaunchDescriptionSource(
-    #             [os.path.join(get_package_share_directory('ros_ign_gazebo'),
-    #                           'launch', 'ign_gazebo.launch.py')]),
-    #         launch_arguments=[('ign_args', [' -r -v 4 empty.sdf'])])
+    # PATHを追加で通さないとSTLファイルが読み込まれない
     env = {'IGN_GAZEBO_SYSTEM_PLUGIN_PATH': os.environ['LD_LIBRARY_PATH'],
-           'IGN_GAZEBO_RESOURCE_PATH': os.path.dirname(get_package_share_directory('crane_plus_description'))}
-
-    world_file = os.path.join(get_package_share_directory('crane_plus_ignition'),
-        'worlds', 'table.sdf')
+           'IGN_GAZEBO_RESOURCE_PATH': os.path.dirname(
+               get_package_share_directory('crane_plus_description'))}
+    world_file = os.path.join(
+        get_package_share_directory('crane_plus_ignition'), 'worlds', 'table.sdf')
+    # -r オプションで起動時にシミュレーションをスタートしないと、コントローラが起動しない
     ign_gazebo = ExecuteProcess(
             cmd=['ign gazebo -r', world_file],
             output='screen',
             additional_env=env,
             shell=True
         )
+
     ignition_spawn_entity = Node(
         package='ros_ign_gazebo',
         executable='create',
@@ -78,21 +47,6 @@ def generate_launch_description():
                    '-z', '1.02',
                    '-allow_renaming', 'true'],
     )
-
-    # gzserver = IncludeLaunchDescription(
-    #         PythonLaunchDescriptionSource([
-    #             get_package_share_directory('gazebo_ros'),
-    #             '/launch/gzserver.launch.py']),
-    #         condition=IfCondition(LaunchConfiguration('server')),
-    #         launch_arguments={'world': world_file}.items(),
-    #     )
-
-    # gzclient = IncludeLaunchDescription(
-    #         PythonLaunchDescriptionSource([
-    #             get_package_share_directory('gazebo_ros'),
-    #             '/launch/gzclient.launch.py']),
-    #         condition=IfCondition(LaunchConfiguration('gui'))
-    #     )
 
     description_loader = RobotDescriptionLoader()
     description_loader.use_gazebo = 'true'
@@ -105,11 +59,6 @@ def generate_launch_description():
                 '/launch/run_move_group.launch.py']),
             launch_arguments={'loaded_description': description}.items()
         )
-
-    # spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-    #                     arguments=['-entity', 'crane_plus', '-x', '0', '-y', '0',
-    #                                '-z', '1.02', '-topic', '/robot_description'],
-    #                     output='screen')
 
     spawn_joint_state_controller = ExecuteProcess(
                 cmd=['ros2 run controller_manager spawner.py joint_state_controller'],
@@ -130,17 +79,9 @@ def generate_launch_description():
             )
 
     return LaunchDescription([
-        # launch_ignition,
         ign_gazebo,
         move_group,
         ignition_spawn_entity,
-        # node_robot_state_publisher
-        # declare_arg_gui,
-        # declare_arg_server,
-        # gzserver,
-        # gzclient,
-        # move_group,
-        # spawn_entity,
         spawn_joint_state_controller,
         spawn_arm_controller,
         spawn_gripper_controller
