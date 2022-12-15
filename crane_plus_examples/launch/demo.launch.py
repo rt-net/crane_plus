858c92ee5757a7cd89fef7b6219554a36b2e3db9
YusukeKato <s1526035cn@s.chibakoudai.jp>
2022-12-15 17:07:49 +0900

    colcon testで指摘された箇所を修正

--- crane_plus_examples/launch/demo.launch.py
+++ crane_plus_examples/launch/demo.launch.py
@@ -15,12 +15,12 @@
 from ament_index_python.packages import get_package_share_directory
 from crane_plus_description.robot_description_loader import RobotDescriptionLoader
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -61,7 +61,8 @@ def generate_launch_description():
             parameters=[
                 {'video_device': '/dev/video2'},
                 {'frame_id': 'camera_color_optical_frame'}
-            ]
+            ],
+            condition=IfCondition(LaunchConfiguration('use_camera'))
         )
 
     return LaunchDescription([
