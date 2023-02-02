2835d77a7c79e6fbd67797c81903a1b874d02341
YusukeKato <s1526035cn@s.chibakoudai.jp>
2023-02-02 15:06:32 +0900

    二行空け

--- crane_plus_examples/launch/demo.launch.py
+++ crane_plus_examples/launch/demo.launch.py
@@ -23,6 +23,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     declare_port_name = DeclareLaunchArgument(
         'port_name',
