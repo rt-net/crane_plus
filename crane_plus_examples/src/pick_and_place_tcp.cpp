7d994c6f063d76a4f298dc5c0ff26a242a326d8d
MASUTANI Yasuhiro <masutani@isc.osakac.ac.jp>
2024-08-30 12:16:59 +0900

    arm_tcpグループを使うサンプルコードを追加

---
+++ crane_plus_examples/src/pick_and_place_tcp.cpp
@@ -0,0 +1,136 @@
+// Copyright 2020 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Reference:
+// https://github.com/ros-planning/moveit2/blob/main/moveit_demo_nodes
+// /run_move_group/src/run_move_group.cpp
+
+#include <cmath>
+
+#include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
+#include "moveit/move_group_interface/move_group_interface.h"
+#include "rclcpp/rclcpp.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
+using MoveGroupInterface = moveit::planning_interface::MoveGroupInterface;
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("pick_and_place");
+
+double to_radians(const double deg_angle)
+{
+  return deg_angle * M_PI / 180.0;
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions node_options;
+  node_options.automatically_declare_parameters_from_overrides(true);
+  auto move_group_arm_node = rclcpp::Node::make_shared("move_group_arm_node", node_options);
+  auto move_group_gripper_node = rclcpp::Node::make_shared("move_group_gripper_node", node_options);
+  // For current state monitor
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(move_group_arm_node);
+  executor.add_node(move_group_gripper_node);
+  std::thread([&executor]() {executor.spin();}).detach();
+
+  MoveGroupInterface move_group_arm(move_group_arm_node, "arm_tcp");
+  move_group_arm.setMaxVelocityScalingFactor(1.0);  // Set 0.0 ~ 1.0
+  move_group_arm.setMaxAccelerationScalingFactor(1.0);  // Set 0.0 ~ 1.0
+
+  MoveGroupInterface move_group_gripper(move_group_gripper_node, "gripper");
+  move_group_gripper.setMaxVelocityScalingFactor(1.0);  // Set 0.0 ~ 1.0
+  move_group_gripper.setMaxAccelerationScalingFactor(1.0);  // Set 0.0 ~ 1.0
+  auto gripper_joint_values = move_group_gripper.getCurrentJointValues();
+  double GRIPPER_DEFAULT = 0.0;
+  double GRIPPER_OPEN = to_radians(-30);
+  double GRIPPER_CLOSE = to_radians(10);
+
+  move_group_arm.setNamedTarget("vertical");
+  move_group_arm.move();
+
+  gripper_joint_values[0] = GRIPPER_DEFAULT;
+  move_group_gripper.setJointValueTarget(gripper_joint_values);
+  move_group_gripper.move();
+
+  // ----- Picking Preparation -----
+  move_group_arm.setNamedTarget("home");
+  move_group_arm.move();
+
+  gripper_joint_values[0] = GRIPPER_OPEN;
+  move_group_gripper.setJointValueTarget(gripper_joint_values);
+  move_group_gripper.move();
+
+  geometry_msgs::msg::Pose target_pose;
+  tf2::Quaternion q;
+  target_pose.position.x = 0.0;
+  target_pose.position.y = -0.18;
+  target_pose.position.z = 0.17;
+  q.setRPY(to_radians(0), to_radians(0), to_radians(-90));
+  target_pose.orientation = tf2::toMsg(q);
+  move_group_arm.setPoseTarget(target_pose);
+  move_group_arm.move();
+
+  target_pose.position.x = 0.0;
+  target_pose.position.y = -0.09;
+  target_pose.position.z = 0.08;
+  q.setRPY(to_radians(0), to_radians(90), to_radians(-90));
+  target_pose.orientation = tf2::toMsg(q);
+  move_group_arm.setPoseTarget(target_pose);
+  move_group_arm.move();
+
+  target_pose.position.z = 0.05;
+  move_group_arm.setPoseTarget(target_pose);
+  move_group_arm.move();
+
+  // Grasp
+  gripper_joint_values[0] = GRIPPER_CLOSE;
+  move_group_gripper.setJointValueTarget(gripper_joint_values);
+  move_group_gripper.move();
+
+  target_pose.position.z = 0.08;
+  move_group_arm.setPoseTarget(target_pose);
+  move_group_arm.move();
+
+  // ----- Placing Preparation -----
+  move_group_arm.setNamedTarget("home");
+  move_group_arm.move();
+
+  target_pose.position.x = 0.24;
+  target_pose.position.y = 0.0;
+  target_pose.position.z = 0.06;
+  q.setRPY(to_radians(0), to_radians(0), to_radians(0));
+  target_pose.orientation = tf2::toMsg(q);
+  move_group_arm.setPoseTarget(target_pose);
+  move_group_arm.move();
+
+  // Release
+  gripper_joint_values[0] = GRIPPER_OPEN;
+  move_group_gripper.setJointValueTarget(gripper_joint_values);
+  move_group_gripper.move();
+
+  // Return to home and vertical pose
+  move_group_arm.setNamedTarget("home");
+  move_group_arm.move();
+  move_group_arm.setNamedTarget("vertical");
+  move_group_arm.move();
+
+  gripper_joint_values[0] = GRIPPER_DEFAULT;
+  move_group_gripper.setJointValueTarget(gripper_joint_values);
+  move_group_gripper.move();
+
+  rclcpp::shutdown();
+  return 0;
+}
