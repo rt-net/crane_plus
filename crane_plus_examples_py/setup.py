e33e9daeb16c5545191e2c7490c9fda00297723d
Nozomi Mizoguchi <mizoguchi@rt-net.jp>
2024-09-20 14:30:20 +0900

    entrypoint.shのパッケージ名の指定修正

--- crane_plus_examples_py/setup.py
+++ crane_plus_examples_py/setup.py
@@ -6,7 +6,8 @@ package_name = 'crane_plus_examples_py'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    # version='0.0.0',
+    version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -24,14 +25,14 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'aruco_detection = package_name.aruco_detectio:main',
-            'color_detection = package_name.color_detection:main',
-            'gripper_control = package_name.gripper_control:main',
-            'joint_values = package_name.joint_values:main',
-            'pick_and_place_tf = package_name.pick_and_place_tf:main',
-            'pick_and_place = package_name.pick_and_place:main',
-            'pose_groupstate= package_name.pose_groupstate:main',
-            'utils = package_name.utils:main',
+            'aruco_detection = crane_plus_examples_py.aruco_detectio:main',
+            'color_detection = crane_plus_examples_py.color_detection:main',
+            'gripper_control = crane_plus_examples_py.gripper_control:main',
+            'joint_values = crane_plus_examples_py.joint_values:main',
+            'pick_and_place_tf = crane_plus_examples_py.pick_and_place_tf:main',
+            'pick_and_place = crane_plus_examples_py.pick_and_place:main',
+            'pose_groupstate= crane_plus_examples_py.pose_groupstate:main',
+            'utils = crane_plus_examples_py.utils:main',
         ],
     },
 )
