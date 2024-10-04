1c3b113978e397513c4a1afc7d1d8cb64e4e9955
mizonon <mizoguchi@rt-net.jp>
2024-10-04 11:30:49 +0900

    レビュー結果反映

--- crane_plus_moveit_config/launch/run_move_group.launch.py
+++ crane_plus_moveit_config/launch/run_move_group.launch.py
@@ -26,31 +26,6 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 from moveit_configs_utils.launches import generate_rsp_launch
 from moveit_configs_utils.launch_utils import DeclareBooleanLaunchArg
 
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, "r") as file:
-            return file.read()
-    except (
-        EnvironmentError
-    ):  # parent of IOError, OSError *and* WindowsError where available
-        return None
-
-
-def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, "r") as file:
-            return yaml.safe_load(file)
-    except (
-        EnvironmentError
-    ):  # parent of IOError, OSError *and* WindowsError where available
-        return None
-
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -74,7 +49,15 @@ def generate_launch_description():
             description="Set the path to rviz configuration file.",
         )
     )
-
+    
+    declare_rviz_config_file = DeclareLaunchArgument(
+        'rviz_config_file',
+        default_value=get_package_share_directory('crane_plus_moveit_config') + '/launch/run_move_group.rviz',
+        description='Set the path to rviz configuration file.'
+    )
+    
+    ld.add_action(declare_rviz_config_file)
+    
     moveit_config = (
         MoveItConfigsBuilder("crane_plus")
         .planning_scene_monitor(
@@ -112,7 +95,6 @@ def generate_launch_description():
 
     # Move group
     ld.add_entity(generate_move_group_launch(moveit_config))
-
     # RViz
     ld.add_entity(generate_moveit_rviz_launch(moveit_config))
 
