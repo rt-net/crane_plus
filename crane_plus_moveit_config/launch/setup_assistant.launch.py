a74f3b74eb078fb62c3392d7e9a88e4790eac0e8
mizonon <mizoguchi@rt-net.jp>
2024-10-22 12:24:20 +0900

    CI対応(setup_assistant.launch.py)

--- crane_plus_moveit_config/launch/setup_assistant.launch.py
+++ crane_plus_moveit_config/launch/setup_assistant.launch.py
@@ -18,5 +18,7 @@ from moveit_configs_utils.launches import generate_setup_assistant_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("crane_plus", package_name="crane_plus_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder(
+        'crane_plus', package_name='crane_plus_moveit_config'
+        ).to_moveit_configs()
     return generate_setup_assistant_launch(moveit_config)
