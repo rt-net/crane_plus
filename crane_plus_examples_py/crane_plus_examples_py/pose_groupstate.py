f2fc690a4d792c2977d683a60376232f01cd3322
Atsushi Kuwagata <kuwagata@rt-net.jp>
2025-01-17 14:42:24 +0900

    コーディングスタイルを統一

--- crane_plus_examples_py/crane_plus_examples_py/pose_groupstate.py
+++ crane_plus_examples_py/crane_plus_examples_py/pose_groupstate.py
@@ -14,71 +14,67 @@
 
 from crane_plus_examples_py.utils import plan_and_execute
 
-# moveit python library
 from moveit.planning import (
     MoveItPy,
     PlanRequestParameters,
 )
+
 import rclpy
 from rclpy.logging import get_logger
 
 
 def main(args=None):
-    # ros2の初期化
     rclpy.init(args=args)
-
-    # ロガー生成
     logger = get_logger('pose_groupstate')
 
-    # MoveItPy初期化
-    crane_plus = MoveItPy(node_name='moveit_py')
-    crane_plus_arm = crane_plus.get_planning_component('arm')
+    # instantiate MoveItPy instance and get planning component
+    crane_plus = MoveItPy(node_name='pose_groupstate')
     logger.info('MoveItPy instance created')
 
-    # planningのパラメータ設定
+    # アーム制御用 planning component
+    arm = crane_plus.get_planning_component('arm')
+
     plan_request_params = PlanRequestParameters(
         crane_plus,
         'ompl_rrtc',
     )
 
-    # 速度＆加速度のスケーリングファクタを設定
+    # 動作速度の調整
     plan_request_params.max_acceleration_scaling_factor = 1.0  # Set 0.0 ~ 1.0
     plan_request_params.max_velocity_scaling_factor = 1.0  # Set 0.0 ~ 1.0
 
-    # 現在の位置から'home'ポジションに動かす
-    crane_plus_arm.set_start_state_to_current_state()
-    crane_plus_arm.set_goal_state(configuration_name='home')
+    # SRDF内に定義されている'home'の姿勢にする
+    arm.set_start_state_to_current_state()
+    arm.set_goal_state(configuration_name='home')
     plan_and_execute(
         crane_plus,
-        crane_plus_arm,
+        arm,
         logger,
         single_plan_parameters=plan_request_params,
     )
 
-    # 現在の位置から'vertical'ポジションに動かす
-    crane_plus_arm.set_start_state_to_current_state()
-    crane_plus_arm.set_goal_state(configuration_name='vertical')
+    # SRDF内に定義されている'vertical'の姿勢にする
+    arm.set_start_state_to_current_state()
+    arm.set_goal_state(configuration_name='vertical')
     plan_and_execute(
         crane_plus,
-        crane_plus_arm,
+        arm,
         logger,
         single_plan_parameters=plan_request_params,
     )
 
-    # 現在の位置から'home'ポジションに動かす
-    crane_plus_arm.set_start_state_to_current_state()
-    crane_plus_arm.set_goal_state(configuration_name='home')
+    # SRDF内に定義されている'home'の姿勢にする
+    arm.set_start_state_to_current_state()
+    arm.set_goal_state(configuration_name='home')
     plan_and_execute(
         crane_plus,
-        crane_plus_arm,
+        arm,
         logger,
         single_plan_parameters=plan_request_params,
     )
 
-    # MoveItPyの終了
-    crane_plus.shutdown()
-
-    # rclpyの終了
+    # Finish with error. Related Issue
+    # https://github.com/moveit/moveit2/issues/2693
     rclpy.shutdown()
 
 
