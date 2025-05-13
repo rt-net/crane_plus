c816bc970bb45d374e66c9bddb77692001776b99
YusukeKato <yusuke.kato@rt-net.jp>
2025-05-13 17:36:11 +0900

    launchファイル以外のコミットを戻す

--- crane_plus_description/test/test_robot_description_loader.py
+++ crane_plus_description/test/test_robot_description_loader.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from crane_plus_description.robot_description_loader import (
-    RobotDescriptionLoader,
-)
+from crane_plus_description.robot_description_loader import RobotDescriptionLoader
 from launch.launch_context import LaunchContext
 import pytest
 
